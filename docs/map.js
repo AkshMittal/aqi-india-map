cbe11433ad15b69b43941f4f17a9d517d25c71a4
AkshMittal <AkshMittal@users.noreply.github.com>
2025-12-27 20:33:09 +0530

    add legend

--- docs/map.js
+++ docs/map.js
@@ -1,4 +1,21 @@
 const map = L.map('map').setView([22.5, 80], 5);
+const legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function () {
+  const div = L.DomUtil.create("div", "aqi-legend");
+  div.innerHTML = `
+    <strong>AQI</strong><br>
+    <i style="background:#00e400"></i> 0–50<br>
+    <i style="background:#ffff00"></i> 51–100<br>
+    <i style="background:#ff7e00"></i> 101–200<br>
+    <i style="background:#ff0000"></i> 201–300<br>
+    <i style="background:#8f3f97"></i> 301–500
+  `;
+  return div;
+};
+
+legend.addTo(map);
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
@@ -19,7 +36,7 @@ async function updateAQI() {
   const heat = points.map(p => [
     p.lat,
     p.lon,
-    Math.min(p.aqi / 200, 1)
+    Math.sqrt(Math.min(p.aqi / 500, 1))
   ]);
 
   heatLayer.setLatLngs(heat);
