b040ccac28bf49aaa1e7ed9b1bd82fbe9c2c46da
AkshMittal <AkshMittal@users.noreply.github.com>
2025-12-27 20:59:40 +0530

    fix legend and scale

--- docs/map.js
+++ docs/map.js
@@ -1,33 +1,38 @@
 const map = L.map('map').setView([22.5, 80], 5);
 const legend = L.control({ position: "bottomright" });
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
 legend.onAdd = function () {
   const div = L.DomUtil.create("div", "aqi-legend");
   div.innerHTML = `
-    <strong>AQI</strong><br>
-    <i style="background:#00e400"></i> 0–50<br>
-    <i style="background:#ffff00"></i> 51–100<br>
-    <i style="background:#ff7e00"></i> 101–200<br>
-    <i style="background:#ff0000"></i> 201–300<br>
-    <i style="background:#8f3f97"></i> 301–500
+    <strong>AQI (relative intensity)</strong><br>
+    <i style="background:#2c7bb6"></i> Low<br>
+    <i style="background:#00ffcc"></i> Moderate<br>
+    <i style="background:#ffff00"></i> High<br>
+    <i style="background:#ff7e00"></i> Very High<br>
+    <i style="background:#ff0000"></i> Severe
   `;
   return div;
 };
 
 legend.addTo(map);
 
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: '© OpenStreetMap'
-}).addTo(map);
-
 const heatLayer = L.heatLayer([], {
   radius: 40,
   blur: 25,
-  maxZoom: 6,
-  willReadFrequently: true
+  maxZoom: 6
 }).addTo(map);
+
+map.on("zoomend", () => {
+  const z = map.getZoom();
+  heatLayer.setOptions({
+    radius: z < 6 ? 15 : 25
+  });
+});
 
 async function updateAQI() {
   const res = await fetch('https://aqi-data-fetch.akshmittal006.workers.dev/aqi-india-30d');
@@ -36,7 +41,8 @@ async function updateAQI() {
   const heat = points.map(p => [
     p.lat,
     p.lon,
-    Math.sqrt(Math.min(p.aqi / 500, 1))
+    Math.sqrt(Math.min(p.aqi / 500, 1), 0.4)
+
   ]);
 
   heatLayer.setLatLngs(heat);
